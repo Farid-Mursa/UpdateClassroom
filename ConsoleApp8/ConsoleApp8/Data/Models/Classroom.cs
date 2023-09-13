9c9cc7b5ad3a60d40fe51fe6f19b6f699342640d
Farid-Mursa <117182057+Farid-Mursa@users.noreply.github.com>
2023-09-13 16:56:14 +0400

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Data/Models/Classroom.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp8.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8.Data.Models
+{
+    public class Classroom : BaseModel
+    {
+        private static int id = 0;
+
+        public Classroom()
+        {
+            Id = id;
+            id++;
+        }
+
+        public string Name { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public List<Student> Students { get; set; } = new();
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Data/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp8.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8.Data.Models
+{
+    public class Student : BaseModel
+    {
+        private static int id = 0;
+
+        public Student()
+        {
+            Id = id;
+            id++;
+        }
+
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public DateOnly Birthday { get; set; }
+    }
+}
